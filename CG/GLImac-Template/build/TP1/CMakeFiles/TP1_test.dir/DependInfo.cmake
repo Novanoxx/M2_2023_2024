5e4f9c47b8d636dc1d739d845ba3f90ffc0f3fe9
StephaneV <NovanoxCorp@outlook.fr>
2024-03-12 16:40:20 +0100

    Update Morpho repo

---
+++ CG/GLImac-Template/build/TP1/CMakeFiles/TP1_test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stephanev/Documents/M2_2023_2024/CG/GLImac-Template/TP1/test.cpp" "TP1/CMakeFiles/TP1_test.dir/test.cpp.o" "gcc" "TP1/CMakeFiles/TP1_test.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/stephanev/Documents/M2_2023_2024/CG/GLImac-Template/build/glimac/CMakeFiles/glimac.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CG/GLImac-Template/build/TP1/CMakeFiles/TP1_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1_test.dir/test.cpp.o"
+  "CMakeFiles/TP1_test.dir/test.cpp.o.d"
+  "TP1_test"
+  "TP1_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
